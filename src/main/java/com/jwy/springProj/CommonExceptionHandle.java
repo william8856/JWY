2f07a65549d1799e6dcc737fb94d96b12168fee4
Jang10 <william8457@gmail.com>
2021-02-24 10:49:35 +0900

    예외처리 클래스 수정

--- src/main/java/com/jwy/springProj/CommonExceptionHandle.java
+++ src/main/java/com/jwy/springProj/CommonExceptionHandle.java
@@ -4,16 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class CommonExceptionHandle {
 	private static final Logger logger = LoggerFactory.getLogger(CommonExceptionHandle.class);
 	
+//	@ExceptionHandler
+//	public String commonError(Exception ex) {
+//		logger.info(ex.toString());
+//		return "error";
+//	}
+	
 	@ExceptionHandler
-	public String commonError(Exception ex) {
+	public ModelAndView commonError(Exception ex) {
 		logger.info(ex.toString());
 		
-		
-		return "error";
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("error");  // "error"를 ViewResolver(servlet-context.xml) 객체에 전송하여 /WEB-INF/views/error.jsp를 찾게 만든다.
+		mav.addObject("exception", ex);  // mav에 ex객체 바인딩
+		return mav;
 	}
 }
